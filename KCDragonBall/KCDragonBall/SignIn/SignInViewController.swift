4a9e157013fe9dd8ee8d3ef209471388d88fca5b
Ariana Nagumo <arinagumo@gmail.com>
2025-03-11 17:02:30 -0600

    style(login): create login screen

---
+++ KCDragonBall/KCDragonBall/SignIn/SignInViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SignInViewController.swift
+//  KCDragonBall
+//
+//  Created by Ariana Rodríguez on 11/03/25.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
